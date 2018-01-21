bb779ea89c77bf77d58fdb1864a2df860439efb7
fdambrine <francois.dambrine@vadesecure.com>
2018-01-21 17:47:47 +0100

    correction tp1

--- tp1/index.php
+++ tp1/index.php
@@ -12,10 +12,14 @@ if(!isset($_SESSION['game'])) {
 	$nbRow = isset($_GET['row']) && filter_var($_GET['row'], FILTER_SANITIZE_NUMBER_INT) ? filter_var($_GET['row'], FILTER_SANITIZE_NUMBER_INT): 25;
 	$nbCol = isset($_GET['col']) && filter_var($_GET['col'], FILTER_SANITIZE_NUMBER_INT) ? filter_var($_GET['col'], FILTER_SANITIZE_NUMBER_INT): 25;
 	$_SESSION['game'] = initializeGame($nbRow, $nbCol);
-} else {
-	$oldGame = $_SESSION['game'];
-	$_SESSION['game'] = nextGeneration($_SESSION['game']);
-	$started = !gameIsStill($oldGame, $_SESSION['game']);
-	$_SESSION['generation']++;
+} 
+if((isset($_GET['nb_generation']) && filter_var($_GET['nb_generation'], FILTER_SANITIZE_NUMBER_INT)) || $_SESSION['generation'] > 0) {
+	$loops = (isset($_GET['nb_generation']) && filter_var($_GET['nb_generation'], FILTER_SANITIZE_NUMBER_INT)) ? (int)$_GET['nb_generation'] : 1;
+	for($i = 0; $i < $loops; $i++) {
+		$oldGame = $_SESSION['game'];
+		$_SESSION['game'] = nextGeneration($_SESSION['game']);
+		$started = !gameIsStill($oldGame, $_SESSION['game']);
+		$_SESSION['generation']++;
+	}
 }
 include('template.html');
