0c5c5bc1f2450edfe2be1cc98fd65e854529186f
Thiebosh <thibaut.juzeau1@gmail.com>
2018-03-14 23:10:24 +0100

    bases

--- tp4/controller.php
+++ tp4/controller.php
@@ -6,14 +6,8 @@ if (!isset($_SESSION['logged'])) {
     require('data/dataLogin.php');
 }
 else {
-    if ($_SESSION['rank'] == "CUSTOMER") {
-        require('view/viewCustomer.php');
-        require('data/dataProcessing.php');
-    }
-    else {
-        require('view/viewOrganizer.php');
-        require('data/dataProcessing.php');
-    }
+    require('view/viewCalendar.php');
+    require('data/dataProcessing.php');
 }
 
 if (isset($_POST["logged"]) && $_POST["logged"] == false) {//si user se deconnecte
