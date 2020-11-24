a62ec1e2c4c083b7d99f3fafd4b2e2815c8b66d7
Thibaut Juzeau <39088822+Thiebosh@users.noreply.github.com>
2020-11-24 12:26:30 +0100

    Update mCommon.php

--- project/Model/mCommon.php
+++ project/Model/mCommon.php
@@ -6,7 +6,7 @@ function dbConnect() {
     $errMsg = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
     $dbName = 'eventCalendar';
     $dbUser = 'root';
-    $dbPass = '1a9z9e8r';
+    $dbPass = '';
     
     $dataBase = new PDO('mysql:host=localhost;dbname='.$dbName.';charset=utf8', $dbUser, $dbPass, $errMsg);
     if (!$dataBase) throw new Exception("Base De Données : Echec de connexion");
